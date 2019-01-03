498f596d4d6c3bd5d10c4684f32ba619453cfb81
Isaac Falkenstine <isaac@falkenstine.com>
2019-01-03 08:50:28 -0700

    forecast request is successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :forecasts, only: [:index]
+      resources :forecast, only: [:index]
     end
   end
 end
