c6ec2b92414a11d161443186bf06a7d0b99b2632
Isaac Falkenstine <isaac@falkenstine.com>
2019-01-04 11:44:15 -0700

    Refactoring (WIP)
    I refactored my code by making gif_urls, summaries, and times all their own classes

--- app/models/gifs.rb
+++ app/models/gifs.rb
@@ -3,13 +3,19 @@ class Gifs
   attr_reader :id,
               :daily_forecasts,
               :location,
-              :copyright
+              :copyright,
+              :times,
+              :summaries,
+              :gif_urls
 
   def initialize(location)
     @location = location
     @id = darksky_service.right_now_forcast[:time]
     @daily_forecasts = make_days
     @copyright = "2018"
+    @times = Times.new(darksky_service).times
+    @summaries = Summaries.new(darksky_service).summaries
+    @gif_urls = GifUrls.new(@summaries).gif_urls
   end
 
   def location_coords
@@ -20,28 +26,7 @@ class Gifs
     DarkSkyService.new(location_coords[:lat], location_coords[:lng])
   end
 
-  def times
-    @times ||= darksky_service.weekly_forcast.map do |day|
-      day[:time]
-    end
-  end
-
-  def summaries
-    @summaries ||= darksky_service.weekly_forcast.map do |day|
-      day[:summary]
-    end
-  end
-
-  def gif_urls
-    @gif_urls ||= summaries.map do |summary|
-      GiphyService.new(summary).url
-    end
-  end
-
   def make_days
-    times
-    summaries
-    gif_urls
     5.times.collect do |int|
       {time: @times[int],
        summary: @summaries[int],
