773b76be7e4b247bb5dfb900f172a3a081181558
Isaac Falkenstine <isaac@falkenstine.com>
2019-01-04 11:54:32 -0700

    Fixed refactored code and fixed tests
    My tests were using outdated methods and the attributes were wrong, and I had to move the "make_days" method in my initialize because it didn't know what times, summaries or gif_urls were.

--- app/models/gifs.rb
+++ app/models/gifs.rb
@@ -11,11 +11,11 @@ class Gifs
   def initialize(location)
     @location = location
     @id = darksky_service.right_now_forcast[:time]
-    @daily_forecasts = make_days
     @copyright = "2018"
     @times = Times.new(darksky_service).times
     @summaries = Summaries.new(darksky_service).summaries
     @gif_urls = GifUrls.new(@summaries).gif_urls
+    @daily_forecasts = make_days
   end
 
   def location_coords
@@ -28,9 +28,9 @@ class Gifs
 
   def make_days
     5.times.collect do |int|
-      {time: @times[int],
-       summary: @summaries[int],
-       url: @gif_urls[int]
+      {time: times[int],
+       summary: summaries[int],
+       url: gif_urls[int]
       }
     end
   end
--- spec/requests/api/v1/giphy_request_spec.rb
+++ spec/requests/api/v1/giphy_request_spec.rb
@@ -5,8 +5,13 @@ describe "Giphy API" do
     get '/api/v1/gifs?location=denver,co'
 
     expect(response).to be_successful
-    expect(response[:data][:daily_forecasts].first).to have_key(:time)
-    expect(response[:data][:daily_forecasts].first).to have_key(:summary)
-    expect(response[:data][:daily_forecasts].first).to have_key(:url)
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:data]).to have_key(:attributes)
+    expect(parsed[:data][:attributes]).to have_key(:daily_forecasts)
+    expect(parsed[:data][:attributes][:daily_forecasts].first).to have_key(:url)
+    expect(parsed[:data][:attributes][:daily_forecasts].first).to have_key(:time)
+    expect(parsed[:data][:attributes][:daily_forecasts].first).to have_key(:summary)
   end
 end
