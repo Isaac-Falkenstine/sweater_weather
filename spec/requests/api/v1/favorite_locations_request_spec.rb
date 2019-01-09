4c30d672a3f837c0b6e956d4d8253f10988c658e
Isaac Falkenstine <isaac@falkenstine.com>
2019-01-08 19:10:19 -0700

    Added sad path test for deleting favorites

--- spec/requests/api/v1/favorite_locations_request_spec.rb
+++ spec/requests/api/v1/favorite_locations_request_spec.rb
@@ -90,4 +90,22 @@ describe 'favorite locations and response' do
     expect(parsed.first).to have_key(:current_weather)
     expect(parsed.first[:location]).to eq("bolder,co")
   end
+
+  it '/api/v1/favorites DELETE no api_key' do
+    post '/api/v1/users?email=email_address@example.com&password=password&password_confirmation=password'
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    post "/api/v1/favorites?location=denver,co&api_key=#{parsed[:data][:attributes][:api_key]}"
+    post "/api/v1/favorites?location=bolder,co&api_key=#{parsed[:data][:attributes][:api_key]}"
+
+    delete "/api/v1/favorites?location=denver,co&api_key=ThisIsNOTAVaildKey"
+
+    expect(response).not_to be_successful
+    expect(status).to eq(401)
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:error]).to eq("Unauthorized")
+  end
 end
